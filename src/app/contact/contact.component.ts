8143be2df9ded8d77f3a7d8f25700918a0ab1c9c
Jitendra Shrestha <Jitendra.Shrestha@Staples.com>
2018-02-05 12:27:47 -0500

    Route and ActivatedRoute

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe( res => console.log(res.contactNumber));
+  }
+
+  ngOnInit() {
+  }
+
+}
