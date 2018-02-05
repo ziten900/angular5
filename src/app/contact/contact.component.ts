34466fab4b140b8a241869d86f552fe17e43f4b5
Jitendra Shrestha <Jitendra.Shrestha@Staples.com>
2018-02-05 13:00:03 -0500

    Router Navigator

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -8,11 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ContactComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     this.route.params.subscribe( res => console.log(res.contactNumber));
   }
 
   ngOnInit() {
+  }
+
+  goToHomes(){
+    this.router.navigate(['']);
   }
 
 }
