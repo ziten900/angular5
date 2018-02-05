f3742153710d78889c72f7bb4088ad819de2aab2
Jitendra Shrestha <Jitendra.Shrestha@Staples.com>
2018-02-05 11:46:03 -0500

    Forms and Basics Interpolations
    1. One way binding : : [value]="btnText"
    2. Two way binding : : [(ngModel)]="goalText"
    3. Event binding : : (click)="addItem()"

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number;
+  btnText: string = 'Add Item!';
+  goalText: string = 'My first life goal';
+  goals = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
