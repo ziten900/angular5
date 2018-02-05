680254f1a1bffe5af7d348efd00614cb095f998b
Jitendra Shrestha <Jitendra.Shrestha@Staples.com>
2018-02-05 12:04:01 -0500

    Animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0})
+          ]))
+        ]), {optional: true}),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)',     offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1.0})
+          ]))
+        ]), {optional: true})
+          
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
@@ -19,8 +43,15 @@ export class HomeComponent implements OnInit {
   }
 
   addItem(){
-    this.goals.push(this.goalText);
-    this.goalText = '';
+    if(this.goalText != ''){
+      this.goals.push(this.goalText);
+      this.goalText = '';
+      this.itemCount = this.goals.length;
+    }
+  }
+
+  removeItem(indexId){
+    this.goals.splice(indexId,1);
     this.itemCount = this.goals.length;
   }
 
